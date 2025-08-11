ce7cdc54bfecb1665f21790ae9e5d573ecac1f5c
Jose Arriola <jose.arriola@dlwlatam.com>
2025-08-11 10:46:54 -0500

    merge

--- sicoes-web/src/app/service/personal-reemplazo.service.ts
+++ sicoes-web/src/app/service/personal-reemplazo.service.ts
@@ -1,87 +1,88 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ConfigService } from '../core/services';
-import { PersonalReemplazo } from '../interface/reemplazo-personal.model';
-import { Pageable } from '../interface/pageable.model';
-import { SeccionReemplazoPersonal } from '../interface/seccion.model';
-import { functions } from 'src/helpers/functions';
-import { id } from 'date-fns/locale';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { ConfigService } from "../core/services";
+import { Pageable } from "../interface/pageable.model";
+import { PersonalReemplazo } from "../interface/reemplazo-personal.model";
+import { SeccionReemplazoPersonal } from "../interface/seccion.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PersonalReemplazoService {
-
   private _path_serve: String;
 
-  constructor(
-    private http: HttpClient,
-    private configService: ConfigService
-  ) {
+  constructor(private http: HttpClient, private configService: ConfigService) {
     this._path_serve = this.configService.getAPIUrl();
   }
 
   listarPersonalReemplazo(idSolicitud: number): Observable<any> {
-    const params = new HttpParams()
-    .set('page', '0')
-    .set('size', '100');
+    const params = new HttpParams().set("page", "0").set("size", "100");
 
     const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/obtener/${idSolicitud}`;
     return this.http.get<Pageable<any>>(urlEndpoint, { params });
   }
 
   eliminarPersonalReemplazo(idReemplazo: number): Observable<any> {
-    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/${idReemplazo}`
+    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/${idReemplazo}`;
     return this.http.delete(urlEndpoint);
   }
 
   listarSeccionesPersonalReemplazo(): Observable<SeccionReemplazoPersonal[]> {
-    const urlEndpoint = `${this._path_serve}/api/listado/SECCIONES_REEMPLAZO_PERSONAL`
+    const urlEndpoint = `${this._path_serve}/api/listado/SECCIONES_REEMPLAZO_PERSONAL`;
     return this.http.get<SeccionReemplazoPersonal[]>(urlEndpoint);
   }
 
   listarSupervisoraPerfil(idPropuesta: number): Observable<any> {
     const params = new HttpParams()
-    .set('idPropuesta', idPropuesta)
-    .set('page', '0')
-    .set('size', '10');
+      .set("idPropuesta", idPropuesta)
+      .set("page", "0")
+      .set("size", "10");
 
     const urlEndpoint = `${this._path_serve}/api/supervisora-perfiles/profesionales/propuesto/baja`;
     return this.http.get<Pageable<any>>(urlEndpoint, { params });
   }
 
-  guardarBajaPersonal(personalBaja: Partial<PersonalReemplazo>): Observable<any> {
+  guardarBajaPersonal(
+    personalBaja: Partial<PersonalReemplazo>
+  ): Observable<any> {
     const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/baja/inserta/propuesto`;
-    
+
     return this.http.post<any>(urlEndpoint, personalBaja);
   }
 
-  eliminarBajaPersonal(idReemplazo: number, idSolicitud: number): Observable<any> {
+  eliminarBajaPersonal(
+    idReemplazo: number,
+    idSolicitud: number
+  ): Observable<any> {
     const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/baja/elimina/propuesto`;
     const body = {
       idReemplazo: idReemplazo,
-      idSolicitud: idSolicitud
+      idSolicitud: idSolicitud,
     };
-    
+
     return this.http.put<any>(urlEndpoint, body);
   }
 
   listarSupervisoraApto(idPerfil: number): Observable<any> {
     const params = new HttpParams()
-    .set('idPerfil', idPerfil)
-    .set('page', '0')
-    .set('size', '10');
+      .set("idPerfil", idPerfil)
+      .set("page", "0")
+      .set("size", "10");
 
     const urlEndpoint = `${this._path_serve}/api/supervisora-perfiles/profesionales/propuesto/reemplazo`;
     return this.http.get<Pageable<any>>(urlEndpoint, { params });
   }
 
   adjuntarArchivo(formData: any): Observable<any> {
-    return this.http.post<any>(`${this._path_serve}/api/documentosreemplazo`, formData, {
-      reportProgress: true,
-      observe: 'events'
-    });
+    return this.http.post<any>(
+      `${this._path_serve}/api/documentosreemplazo`,
+      formData,
+      {
+        reportProgress: true,
+        observe: "events",
+      }
+    );
   }
 
   eliminarAdjunto(idDocumento: number): Observable<any> {
@@ -90,21 +91,21 @@ export class PersonalReemplazoService {
   }
 
   guardarPersonalPropuesto(data: any): Observable<any> {
-    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/propuesta/inserta/propuesto`;    
+    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/propuesta/inserta/propuesto`;
     return this.http.put<any>(urlEndpoint, data);
   }
 
   eliminarPersonalPropuesto(data: any): Observable<any> {
-    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/propuesta/elimina/propuesto`;    
+    const urlEndpoint = `${this._path_serve}/api/externo/reemplazo/solicitud/propuesta/elimina/propuesto`;
     return this.http.put<any>(urlEndpoint, data);
   }
 
   listarDocsReemplazo(idReemplazo: number): Observable<any> {
     const urlEndpoint = `${this._path_serve}/api/documentosreemplazo`;
     const params = new HttpParams()
-    .set('idReemplazo', idReemplazo)
-    .set('page', '0')
-    .set('size', '100');
+      .set("idReemplazo", idReemplazo)
+      .set("page", "0")
+      .set("size", "100");
 
     return this.http.get<Pageable<any>>(urlEndpoint, { params });
   }
@@ -121,13 +122,18 @@ export class PersonalReemplazoService {
 
   grabaConformidad(data: any): Observable<any> {
     const urlEndpoint = `${this._path_serve}/api/reemplazo/solicitud/propuesto/revisa`;
-    
+
     return this.http.post<any>(urlEndpoint, data);
   }
 
   guardarRevDocumentos(data: any): Observable<any> {
     const urlEndpoint = `${this._path_serve}/api/reemplazo/solicitud/registra/propuesto/revision`;
-    
+
     return this.http.post<any>(urlEndpoint, data);
+  }
+
+  obtenerSeccionAdenda(idReemplazo: number, seccion: string): Observable<any> {
+    const urlEndpoint = `${this._path_serve}/documentosreemplazo/seccion?idReemplazo=${idReemplazo}&seccion=${seccion}`;
+    return this.http.get<any>(urlEndpoint);
   }
 }

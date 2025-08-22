de42c23f960f6431b3bd1e96d504a61511ff1f4b
Patricio López <patricio.lopez@dlwlatam.com>
2025-08-21 19:04:32 -0500

    Se agrega secuencia 14

--- sicoes-web/src/app/shared/layout-acta-inicio/layout-acta-inicio.component.ts
+++ sicoes-web/src/app/shared/layout-acta-inicio/layout-acta-inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 import { BaseComponent } from '../components/base.component';
 
 @Component({
@@ -9,15 +9,46 @@ import { BaseComponent } from '../components/base.component';
 export class LayoutActaInicioComponent extends BaseComponent implements OnInit {
 
   @Input() isReview: boolean;
+  @Input() personalReemplazo: any;
+
+  @Output() seccionCompletada = new EventEmitter<boolean>();
+  @Output() fechaInicio = new EventEmitter<string>();
 
   editable: boolean = true;
-  marcacion: 'si' | 'no' | null = null;
+  adjuntoCargadoActaInicio: boolean = false;
+
+  fechaSeleccionada: string = null;
+
+  marcacion: 'SI' | 'NO' | null = null;
 
   constructor() {
     super();
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['personalReemplazo'] && changes['personalReemplazo'].currentValue) {
+      const nuevoPersonalReemplazo = changes['personalReemplazo'].currentValue;
+      this.personalReemplazo = nuevoPersonalReemplazo;
+    }
+  }
+
+  onActaInicioAdjunto(valor: boolean) {
+    this.adjuntoCargadoActaInicio = valor;
+    this.seccionCompletada.emit(this.validarSeccionCompletada());
+  }
+
+  onFechaChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.fechaSeleccionada = input.value;
+    this.fechaInicio.emit(this.fechaSeleccionada);
+    this.seccionCompletada.emit(this.validarSeccionCompletada());
+  }
+
+  validarSeccionCompletada() {
+    return this.adjuntoCargadoActaInicio && !!this.fechaSeleccionada;
   }
 
   setValueCheckedContratoLab(even) {

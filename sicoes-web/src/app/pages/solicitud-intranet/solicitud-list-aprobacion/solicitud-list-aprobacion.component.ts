33d6013bc713041081d231101179e1b8dd287872
DLW <fabrica_administrativos@dlwlatam.com>
2025-10-03 17:00:54 -0500

    fix(campo): se actualizan los campos mostrados en la tabla de historial de aprobaciones y se ajusta la funcionalidad del botón de historial. Se corrigen referencias a propiedades en el componente.

--- sicoes-web/src/app/pages/solicitud-intranet/solicitud-list-aprobacion/solicitud-list-aprobacion.component.ts
+++ sicoes-web/src/app/pages/solicitud-intranet/solicitud-list-aprobacion/solicitud-list-aprobacion.component.ts
@@ -962,15 +962,19 @@ historyApproveAndSignPerfeccionamiento(row: any) {
   }
 
   pageChangeHistorialInformeRenovacion(event: any) {
-    this.historialAprobaciones();
+    // this.historialAprobaciones();
   }
 
-  historialAprobaciones(){
+  historialAprobaciones(row: any){
     this.mostrarHistorial = !this.mostrarHistorial;
     this.mostrarCards = !this.mostrarCards;
 
-    const filtro = this.obtenerFiltroHistorialInformeRenovacion();
-    console.log('historialAprobaciones', filtro);
+    // const filtro = this.obtenerFiltroHistorialInformeRenovacion();
+    // console.log('historialAprobaciones', filtro);
+
+    const filtro = {
+      idInformeRenovacion: row.idInformeRenovacion
+    }
 
     this.dataSourceHistorial.data = [];
     this.isLoading = true;
@@ -979,7 +983,7 @@ historyApproveAndSignPerfeccionamiento(row: any) {
       .subscribe(
         (data) => {
           console.log('historialAprobaciones', JSON.stringify(data));
-          this.dataSourceHistorial.data = data.historial_aprobaciones || [];
+          this.dataSourceHistorial.data = data.content || [];
           if (this.paginatorHistoriaInformeRenovacion) {
             this.paginatorHistoriaInformeRenovacion.length = data.total_registros || 0;
           }
